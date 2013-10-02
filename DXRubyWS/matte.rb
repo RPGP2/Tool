b7706a7912736c03831e9ea8eb409c319fc5f8d8
RPGP1 <great.super.science@gmail.com>
2013-10-02 19:57:52 +0900

    Update matte.rb

--- DXRubyWS/matte.rb
+++ DXRubyWS/matte.rb
@@ -2,27 +2,29 @@
 
 #「お待ちください」的なウィンドウを出しとく。
 
-module Window
-  def join(content = "処理中です。しばらくお待ちください。", &b)
-    jobEnd = false
-    
-    thread = Thread.new{
-      result = b.call
-      jobEnd = true
-      result
-    }
-    
-    font = Font.new(12)
-    old_cap = Window.caption
-    Window.caption = "処理中"
-    while !jobEnd do
-      Window.gameloop do
-        Window.drawFont((Window.width - font.getWidth(content)) / 2,(Window.height - 12) / 2, content, font)
-        break if jobEnd
+module DXRuby
+  module Window
+    def join(content = "処理中です。しばらくお待ちください。", &b)
+      jobEnd = false
+      
+      thread = Thread.new{
+        result = b.call
+        jobEnd = true
+        result
+      }
+      
+      font = Font.new(12)
+      old_cap = Window.caption
+      Window.caption = "処理中"
+      while !jobEnd do
+        Window.gameloop do
+          Window.drawFont((Window.width - font.getWidth(content)) / 2,(Window.height - 12) / 2, content, font)
+          break if jobEnd
+        end
       end
+      Window.caption = old_cap
+      
+      thread.value
     end
-    Window.caption = old_cap
-    
-    thread.value
   end
 end
